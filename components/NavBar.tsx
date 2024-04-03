926800bf69944724fb398c04710fb59eba3fca6b
Richard Andrew Long <id-dev@Richards-MacBook-Air.local>
2024-04-02 20:07:22 -0500

    NavBar and Menu

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,56 @@
 import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
+import Image from "next/image";
 
-export default function NavBar() {
-  return <div>This is the NavBar</div>;
-}
+const Navbar = () => {
+  return (
+    <div className="h-20 text-white p-4 flex items-center justify-between border-b-2 border-b-purple-200 uppercase md:h-24 lg:px-20 xl:px-40">
+      {/* Left Links */}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link className="hover:text-gray-600" href="/">
+          Home
+        </Link>
+        <Link className="hover:text-gray-600" href="/about">
+          About
+        </Link>
+        <Link className="hover:text-gray-600" href="/services">
+          Services
+        </Link>
+        <Link className="hover:text-gray-600" href="/work">
+          Work
+        </Link>
+      </div>
+
+      {/* {Logo} */}
+      <div className="text-xl md:font-bold flex-1 md:text-center flex justify-center items-center">
+        <Link href="/">
+          <Image
+            src="/laserbrain.webp"
+            alt="laserbrain logo"
+            width={50}
+            height={50}
+            className=""
+          />
+        </Link>
+      </div>
+
+      {/* {Mobile Menu} */}
+      <div className="md:hidden">
+        <Menu />
+      </div>
+      {/* Right Links */}
+      <div className="hidden md:flex flex-1 items-center justify-end">
+        <div className="relative flex items-center gap-2 cursor-pointer">
+          <Link href="/contact">
+            <div className="bg-gradient-to-br from-purple-400 to-sky-300 px-3 py-1 rounded hover:opacity-80 transition duration-300">
+              <span className="text-white">Contact</span>
+            </div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
